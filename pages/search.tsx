04a4651bed4245055865d8f8c891393a275c684c
drsfieldsIMA <57874229+drsfieldsIMA@users.noreply.github.com>
2022-01-27 11:53:32 +0100

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -12,7 +12,6 @@ import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import API_URL, { API_MONGOOSE_URL } from "../utils/index";
 import { useQuery } from "react-query";
-import UseDebounce from "../utils/useDebounce";
 import SearchArticles from "../utils/SearchArticles";
 import ArticlesSearchResult from "./comps/ArticlesSearchResults";
 import Dropdown from "./comps/navigation/Dropdown";
