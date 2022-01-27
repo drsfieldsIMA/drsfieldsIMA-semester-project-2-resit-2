a4e34afa7e41050cd9e312e3b7d99f796d06ab87
drsfieldsIMA <57874229+drsfieldsIMA@users.noreply.github.com>
2022-01-27 01:15:48 +0100

    newest

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import useLocalStorage from "../comps/config/useLocalStorage";
 import { parseCookies  } from 'nookies';
-import { API_URL } from '../comps/config'
+import API_URL from '../comps/config'
 
 type HeadingParams = {
   color?: string;
